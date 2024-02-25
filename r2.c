a2c812f63d024ef96192b35f6dc9fb3efb20d024
appocalypsegames <55316319+appocalypsegames@users.noreply.github.com>
2024-02-25 13:49:21 +0100

    Revert "carrots, hello, r2 and thelasproblem"
    This reverts commit 3e929f22e6a2196ae22cdd085c10e6ee3bb30b0f.

--- r2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-    int r1,r2,s;
-    scanf("%i %i",&r1,&s);
-    printf("%i",-(r1-s*2));
-    return 0;
-}
--- thelastproblem.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-    char s[50];
-    gets(s);
-    printf("Thank you, %s, and farewell!",s);
-    return 0;
-}
